b8872ef466433bac59b8b1c123a6f7d0198d1df8
Jad Driggers <jaddriggers@gmail.com>
2020-09-21 14:09:54 -0400

    Chapter 20 rand() and time_t

--- C_ConsoleApp/C_ConsoleApp.cpp
+++ C_ConsoleApp/C_ConsoleApp.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string>
+#include <time.h>
 
 //int main()
 //{
@@ -972,4 +973,89 @@
 //	return 0;
 //}
 
-//Chapter 20 Advanced math for your computer
+//Chapter 20 Advanced math for your computer rand()
+
+//int main()
+//
+//{
+//	int dice1, dice2;
+//	int total1, total2;
+//	time_t t;
+//	char ans;
+//
+//	//Needed to make sure that each random number is generated is different
+//
+//	srand(time_t(&t));
+//
+//	//this would give you a number between 1 and 5 so the +1 will give you 6
+//
+//	dice1 = (rand() % 5) + 1;
+//	dice2 = (rand() % 5) + 1;
+//	total1 = dice1 + dice2;
+//	printf("First role of the dice was %d and %d. ", dice1, dice2);
+//	printf("For a total of %d\n\n\n", total1);
+//
+//	do
+//	{
+//		puts("What do you think the next roll will be (H)igher, (L)ower, or (S)ame?\n");
+//		puts("Enter H, L, or S to reflect your guess. ");
+//
+//		scanf_s(" %c", &ans);
+//		ans = toupper(ans);
+//	} while ((ans != 'H') && (ans != 'L') && (ans != 'S'));
+//
+//	//roll the dice a second time to get your second total
+//
+//	dice1 = (rand() % 5) + 1;
+//	dice2 = (rand() % 5) + 1;
+//	total2 = dice1 + dice2;
+//	printf("Second role of the dice was %d and %d. ", dice1, dice2);
+//	printf("For a total of %d\n\n\n", total2);
+//
+//	//Conmpare the two roles against the user's guess and tell them if they were right.
+//
+//	//If user enters L
+//	if (ans == 'L')
+//	{
+//		if (total2 < total1)
+//		{
+//			printf("Good job! You guessed correct!\n");
+//			printf("%d is lower than %d\n", total2, total1);
+//		}
+//
+//		else
+//		{
+//			printf("Sorry! %d is not lower than %d\n", total2, total1);
+//		}
+//	}
+//
+//	//if user enters H
+//
+//	else if (ans == 'H')
+//	{
+//		if (total2 > total1)
+//		{
+//			printf("Good job! You guessed correct!\n");
+//			printf("%d is higher than %d\n", total2, total1);
+//		}
+//		else
+//		{
+//			printf("Sorry! %d is not higher than %d\n", total2, total1);
+//		}
+//	}
+//
+//	//If user enters S
+//
+//	else if (ans == 'S')
+//	{
+//		printf("Good job! You guessed correct!\n");
+//		printf("%d is the same as %d\n", total2, total1);
+//	}
+//	else
+//	{
+//		printf("Sorry! %d is not the same as %d\n\n", total2, total1);
+//	}
+//	return 0;
+//}
+
+//Chapter 21-Dealing with Arrays
